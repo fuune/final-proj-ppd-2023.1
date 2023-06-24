320de8568c7f536870319d496163b7d1c9d083b0
fuune <ifuune@gmail.com>
2023-06-24 12:33:56 -0300

    add base client chat app

---
+++ ClientChat/ClientChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClientChat
+//
+//  Created by Daniel Barbosa on 24/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
